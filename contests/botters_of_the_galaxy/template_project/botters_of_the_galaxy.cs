f4215c9cbc7913735783f01e8a4a3a5b91c7e272
Ben Lechlitner <otherland@gmail.com>
2019-01-28 21:02:22 -0600

    updating botters of the galaxy files

--- contests/botters_of_the_galaxy/template_project/botters_of_the_galaxy.cs
+++ contests/botters_of_the_galaxy/template_project/botters_of_the_galaxy.cs
@@ -15,10 +15,6 @@ class Player
         {
             game.Update();
 
-
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
-
             Console.WriteLine(game.Command);
         }
     }
@@ -27,18 +23,19 @@ class Player
 
 public class Game
 {
+    private const bool _consoleLogging = true;
     private const int PlayerCount = 2;
-
+    
     private Entity[] _players;
     private bool _gameRound;
     private bool _heroRound;
 
-    public int ProjectCount { get; private set; }
     public Entity Me => _players[0];
 
     public Game()
     {
         _players = new Entity[PlayerCount];
+        Items = new List<Item>();
 
         for (var i = 0; i < PlayerCount; i++)
         {
@@ -46,46 +43,72 @@ public class Game
         }
 
         // Read initial game state
-        TeamNumber = int.Parse(Console.ReadLine());
+        TeamNumber = ConsoleToInt();
 
-        BushSpawnCount = int.Parse(Console.ReadLine()); // useful from wood1, represents the number of bushes and the number of places where neutral units can spawn
+        BushSpawnCount = ConsoleToInt(); // useful from wood1, represents the number of bushes and the number of places where neutral units can spawn
         for (int i = 0; i < BushSpawnCount; i++)
         {
-            var inputs = Console.ReadLine().Split(' ');
+            var inputs = ConsoleToStrArray();
             string entityType = inputs[0]; // BUSH, from wood1 it can also be SPAWN
             int x = int.Parse(inputs[1]);
             int y = int.Parse(inputs[2]);
             int radius = int.Parse(inputs[3]);
         }
         
-        int itemCount = int.Parse(Console.ReadLine()); // useful from wood2
+        int itemCount = ConsoleToInt(); // useful from wood2
 
         for (int i = 0; i < itemCount; i++)
         {
-            var inputs = Console.ReadLine().Split(' ');
-            string itemName = inputs[0]; // contains keywords such as BRONZE, SILVER and BLADE, BOOTS connected by "_" to help you sort easier
-            int itemCost = int.Parse(inputs[1]); // BRONZE items have lowest cost, the most expensive items are LEGENDARY
-            int damage = int.Parse(inputs[2]); // keyword BLADE is present if the most important item stat is damage
-            int health = int.Parse(inputs[3]);
-            int maxHealth = int.Parse(inputs[4]);
-            int mana = int.Parse(inputs[5]);
-            int maxMana = int.Parse(inputs[6]);
-            int moveSpeed = int.Parse(inputs[7]); // keyword BOOTS is present if the most important item stat is moveSpeed
-            int manaRegeneration = int.Parse(inputs[8]);
-            int isPotion = int.Parse(inputs[9]); // 0 if it's not instantly consumed
+            var inputs = ConsoleToStrArray();
+
+            Items.Add(new Item(inputs[0],            // contains keywords such as BRONZE, SILVER and BLADE, BOOTS connected by "_" to help you sort easier
+                               int.Parse(inputs[1]), // BRONZE items have lowest cost, the most expensive items are LEGENDARY
+                               int.Parse(inputs[2]), // keyword BLADE is present if the most important item stat is damage
+                               int.Parse(inputs[3]), 
+                               int.Parse(inputs[4]),
+                               int.Parse(inputs[5]), 
+                               int.Parse(inputs[6]), 
+                               int.Parse(inputs[7]), // keyword BOOTS is present if the most important item stat is moveSpeed
+                               int.Parse(inputs[8]), 
+                               int.Parse(inputs[9]) != 0)); // 0 if it's not instantly consumed
         }
+    }
+
+    private int ConsoleToInt()
+    {
+        var input = Console.ReadLine();
+
+        if (_consoleLogging)
+        {
+            Console.Error.WriteLine(input);
+        }
+
+        return int.Parse(input);
+    }
+
+    private string[] ConsoleToStrArray()
+    {
+        var input = Console.ReadLine();
+
+        if (_consoleLogging)
+        {
+            Console.Error.WriteLine(input);
+        }
+
+        return input.Split(' ');
     }
 
     public void Update()
     {
-        PlayerGold = int.Parse(Console.ReadLine());
-        OpponentGold = int.Parse(Console.ReadLine());
-        ParseRoundInformation( int.Parse(Console.ReadLine())); // a positive value will show the number of heroes that await a command
-        int entityCount = int.Parse(Console.ReadLine());
+        PlayerGold = ConsoleToInt(); 
+        OpponentGold = ConsoleToInt();
+        ParseRoundInformation(ConsoleToInt()); // a positive value will show the number of heroes that await a command
+        EntityCount = ConsoleToInt();
 
-        for (int i = 0; i < entityCount; i++)
+        for (int i = 0; i < EntityCount; i++)
         {
-            var inputs = Console.ReadLine().Split(' ');
+            var inputs = ConsoleToStrArray();
+
             int unitId = int.Parse(inputs[0]);
             int team = int.Parse(inputs[1]);
             string unitType = inputs[2]; // UNIT, HERO, TOWER, can also be GROOT from wood1
@@ -128,9 +151,6 @@ public class Game
     {
         for (var i = 0; i < PlayerCount; i++)
         {
-            var tmp = Console.ReadLine();
-            Console.Error.WriteLine((string)tmp);
-
             // Parse current status from console input.
         }
     }
@@ -163,6 +183,9 @@ public class Game
     public int ItemCount { get; private set; }
     public int PlayerGold { get; private set; }
     public int OpponentGold { get; private set; }
+    public List<Item> Items { get; private set; }
+    public List<Entity> Entities { get; set; }
+
     public bool GameRound
     {
         get
@@ -188,7 +211,47 @@ public class Game
         }
     }
 
-    public IEnumerable<char> Hero { get; set; }
+    public int EntityCount { get; private set; }
+}
+
+public class Item
+{
+    public Item(string name, int cost, int damage, int health, int maxhealth, int mana, int maxmana, int speed, int regeneration, bool ispotion)
+    {
+        Name = name;
+        Cost = cost;
+        Damage = damage;
+        Health = health;
+        MaxHealth = maxhealth;
+        Mana = mana;
+        MaxMana = maxmana;
+        Speed = speed;
+        Regeneration = regeneration;
+        isPotion = ispotion;
+    }
+
+    public string Name { get; private set; }
+    public int Cost { get; private set; }
+    public int Damage { get; private set; }
+    public int Health { get; private set; }
+    public int MaxHealth { get; private set; }
+    public int Mana { get; private set; }
+    public int MaxMana { get; private set; }
+    public int Speed { get; private set; }
+    public int Regeneration { get; private set; }
+    public bool isPotion { get; private set; }
+}
+public class Hero:Entity
+{
+    public int Cooldown1 { get; set; }
+    public int Cooldown2 { get; set; }
+    public int Cooldown3 { get; set; }
+    public int Mana { get; set; }
+    public int MaxMana { get; set; }
+    public int ManaRegeneration { get; set; }
+    public string HeroType { get; set; }
+    public bool Visible { get; set; }
+    public int ItemCount { get; set; }
 }
 
 public class Entity
@@ -200,7 +263,15 @@ public class Entity
 
     public int Id { get; set; }
     public int Team { get; set; }
-    public int Value { get; set; }
+    public string UnitType { get; set; }
+    public int Range { get; set; }
+    public int Health { get; set; }
+    public int MaxHealth { get; set; }
+    public int Shield { get; set; }
+    public int Damage { get; set; }
+    public int Speed { get; set; }
+    public int StunDuration { get; set; }
+    public int GoldValue { get; set; }
 }
 
 public class Vector
--- contests/botters_of_the_galaxy/template_project_tests/botters_of_the_galaxy_tests.cs
+++ contests/botters_of_the_galaxy/template_project_tests/botters_of_the_galaxy_tests.cs
@@ -35,6 +35,241 @@ namespace template_project_tests
         {
             Assert.Equal(g.ItemCount, 0);
         }
+
+        [Fact]
+        public void Load_Items_Initialization()
+        {
+            Console.SetIn(new StreamReader("../../assets/wood2_single.txt"));
+
+            g = new Game();
+            g.Update();
+
+            Assert.IsType<Item>(g.Items[0]);
+        }
+
+        [Fact]
+        public void Load_ItemBlade_Initialization()
+        {
+            Console.SetIn(new StreamReader("../../assets/wood2_single.txt"));
+
+            g = new Game();
+            g.Update();
+
+            var item = g.Items[0];
+
+            Assert.Equal("Golden_Blade_14", item.Name);
+            Assert.Equal(903, item.Cost);
+            Assert.Equal(56, item.Damage);
+            Assert.Equal(1004, item.Health);
+            Assert.Equal(1004, item.MaxHealth);
+            Assert.Equal(0, item.Mana);
+            Assert.Equal(0, item.MaxMana);
+            Assert.Equal(0, item.Speed);
+            Assert.Equal(0, item.Regeneration);
+            Assert.False(item.isPotion);
+
+        }
+
+        [Fact]
+        public void Load_ItemPotion_Initialization()
+        {
+            Console.SetIn(new StreamReader("../../assets/wood2_single.txt"));
+
+            g = new Game();
+            g.Update();
+
+            var item = g.Items[2];
+
+            Assert.Equal("xxl_potion", item.Name);
+            Assert.Equal(330, item.Cost);
+            Assert.Equal(0, item.Damage);
+            Assert.Equal(500, item.Health);
+            Assert.Equal(0, item.MaxHealth);
+            Assert.Equal(0, item.Mana);
+            Assert.Equal(0, item.MaxMana);
+            Assert.Equal(0, item.Speed);
+            Assert.Equal(0, item.Regeneration);
+            Assert.True(item.isPotion);
+
+        }
+
+        [Fact]
+        public void Load_ItemBoots_Initialization()
+        {
+            Console.SetIn(new StreamReader("../../assets/wood2_single.txt"));
+
+            g = new Game();
+            g.Update();
+
+            var item = g.Items[10];
+
+            Assert.Equal("Bronze_Boots_5", item.Name);
+            Assert.Equal(89, item.Cost);
+            Assert.Equal(5, item.Damage);
+            Assert.Equal(0, item.Health);
+            Assert.Equal(0, item.MaxHealth);
+            Assert.Equal(0, item.Mana);
+            Assert.Equal(0, item.MaxMana);
+            Assert.Equal(15, item.Speed);
+            Assert.Equal(0, item.Regeneration);
+            Assert.False(item.isPotion);
+
+        }
+
+        [Fact]
+        public void Load_ItemGadget_Initialization()
+        {
+            Console.SetIn(new StreamReader("../../assets/wood2_single.txt"));
+
+            g = new Game();
+            g.Update();
+
+            var item = g.Items[12];
+
+            Assert.Equal("Silver_Gadget_8", item.Name);
+            Assert.Equal(376, item.Cost);
+            Assert.Equal(0, item.Damage);
+            Assert.Equal(504, item.Health);
+            Assert.Equal(504, item.MaxHealth);
+            Assert.Equal(100, item.Mana);
+            Assert.Equal(100, item.MaxMana);
+            Assert.Equal(0, item.Speed);
+            Assert.Equal(0, item.Regeneration);
+            Assert.False(item.isPotion);
+
+        }
+
+        [Fact]
+        public void Load_LastItem_Initialization()
+        {
+            Console.SetIn(new StreamReader("../../assets/wood2_single.txt"));
+
+            g = new Game();
+            g.Update();
+
+            var item = g.Items[22];
+
+            Assert.Equal("Legendary_Boots_19", item.Name);
+            Assert.Equal(1340, item.Cost);
+            Assert.Equal(18, item.Damage);
+            Assert.Equal(0, item.Health);
+            Assert.Equal(0, item.MaxHealth);
+            Assert.Equal(100, item.Mana);
+            Assert.Equal(100, item.MaxMana);
+            Assert.Equal(150, item.Speed);
+            Assert.Equal(26, item.Regeneration);
+            Assert.False(item.isPotion);
+
+        }
+
+        [Fact]
+        public void Load_EntityCount_HeroRound()
+        {
+            Console.SetIn(new StreamReader("../../assets/wood2_single.txt"));
+
+            g = new Game();
+            g.Update();
+
+            Assert.Equal(2, g.EntityCount);
+        }
+
+        [Fact]
+        public void Load_EntityCount_GameRound()
+        {
+            Console.SetIn(new StreamReader("../../assets/wood2_single.txt"));
+
+            g = new Game();
+            g.Update();
+            g.Update();
+
+            Assert.Equal(12, g.EntityCount);
+        }
+
+        [Fact]
+        public void Load_Tower_GameRound()
+        {
+            Console.SetIn(new StreamReader("../../assets/wood2_single.txt"));
+
+            g = new Game();
+            g.Update();
+            g.Update();
+            g.Update();
+
+            var tower = g.Entities[0];
+
+            Assert.Equal(1, tower.Id);
+            Assert.Equal(0, tower.Team);
+            Assert.Equal("TOWER", tower.UnitType);
+            Assert.Equal(100, tower.X);
+            Assert.Equal(540, tower.Y);
+            Assert.Equal(400, tower.Range);
+            Assert.Equal(1500, tower.Health);
+            Assert.Equal(1500, tower.MaxHealth);
+            Assert.Equal(0, tower.Shield);
+            Assert.Equal(100, tower.Damage);
+            Assert.Equal(0, tower.Speed);
+            Assert.Equal(0, tower.StunDuration);
+            Assert.Equal(0, tower.GoldValue);
+        }
+
+        [Fact]
+        public void Load_Hero_Initialization()
+        {
+            Console.SetIn(new StreamReader("../../assets/wood2_single.txt"));
+
+            g = new Game();
+            g.Update();
+            g.Update();
+            g.Update();
+
+            var hero = (Hero)g.Entities[0];
+
+            Assert.Equal(3, hero.Id);
+            Assert.Equal(0, hero.Team);
+            Assert.Equal("HERO", hero.UnitType);
+            Assert.Equal(200, hero.X);
+            Assert.Equal(590, hero.Y);
+            Assert.Equal(95, hero.Range);
+            Assert.Equal(1450, hero.Health);
+            Assert.Equal(1450, hero.MaxHealth);
+            Assert.Equal(0, hero.Shield);
+            Assert.Equal(80, hero.Damage);
+            Assert.Equal(200, hero.Speed);
+            Assert.Equal(0, hero.StunDuration);
+            Assert.Equal(300, hero.GoldValue);
+            Assert.Equal(0, hero.Cooldown1);
+            Assert.Equal(0, hero.Cooldown2);
+            Assert.Equal(0, hero.Cooldown3);
+            Assert.Equal(0, hero.Mana);
+            Assert.Equal(90, hero.MaxMana);
+            Assert.Equal(90, hero.ManaRegeneration);
+            Assert.Equal("HULK", hero.HeroType);
+            Assert.True(hero.Visible);
+            Assert.Equal(0, hero.ItemCount);
+        }
+
+        [Fact]
+        public void Load_Opponent_Initialization()
+        {
+            Console.SetIn(new StreamReader("../../assets/wood2_single.txt"));
+
+            g = new Game();
+            g.Update();
+
+            var item = g.Items[22];
+
+            Assert.Equal("Legendary_Boots_19", item.Name);
+            Assert.Equal(1340, item.Cost);
+            Assert.Equal(18, item.Damage);
+            Assert.Equal(0, item.Health);
+            Assert.Equal(0, item.MaxHealth);
+            Assert.Equal(100, item.Mana);
+            Assert.Equal(100, item.MaxMana);
+            Assert.Equal(150, item.Speed);
+            Assert.Equal(26, item.Regeneration);
+            Assert.False(item.isPotion);
+
+        }
         #endregion
 
         #region Hero Round Tests
@@ -75,7 +310,7 @@ namespace template_project_tests
 
         #endregion
 
-        #region Game Round Tests
+    #region Game Round Tests (WOOD3)
 
         [Fact]
         public void Load_RoundType_GAME_ROUND()
@@ -135,5 +370,27 @@ namespace template_project_tests
         }
         #endregion
 
+    #region Game Round Tests (WOOD2)
+
+        [Fact]
+        public void Load_ItemCount_GAME_ROUND()
+        {
+            Console.SetIn(new StreamReader("../../assets/wood2_single.txt"));
+
+            g = new Game();
+            for (int i = 0; i < 2; i++)
+            {
+                g.Update();
+            }
+
+            Assert.Equal(g.ItemCount, 0);
+        }
+
+        #endregion
+
+    #region Game Round Tests (WOOD2)
+
+
+    #endregion
     }
 }
